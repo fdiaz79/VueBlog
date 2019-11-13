412376c713c2663d231dc3cafafe987e63abaccc
Fabian Diaz <16745697+fdiaz79@users.noreply.github.com>
2019-11-12 20:56:01 -0500

    Mixin created. filteredBlogs made mixin

--- vue-blog/src/components/listBlogs.vue
+++ vue-blog/src/components/listBlogs.vue
@@ -1,12 +1,10 @@
 <template>
     <div v-theme:column="'narrow'" id="show-blogs">
-        <h1> All Blog Articles</h1>
+        <h1> List Blog Titles</h1>
         <input type="text" v-model="search" placeholder="search box" />
         <div v-for="blog in filteredBlogs" class="single-blog">
             <h2 v-rainbow>{{blog.title | to-uppercase}} </h2>
-            <article>{{blog.body | snippet}} </article>
-        </div>
-    
+        </div>    
     </div>
 </template>
 
